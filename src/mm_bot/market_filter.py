bde4362cfdfe1f907fc32699a34ab81c58d082ed
ASk <asif142636@gmail.com>
2025-12-10 13:31:12 +0600

    Delete src directory

--- src/mm_bot/market_filter.py
+++
@@ -1,9 +0,0 @@
-def filter_markets(markets):
-    """Only keep binary active markets."""
-    result = []
-    for m in markets:
-        if m.get("outcomeType") != "BINARY":
-            continue
-        if not m.get("isResolved"):
-            result.append(m)
-    return result
